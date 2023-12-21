50b7701291bf1f3a9c26e7254d1c95cbf60668fa
残暴Sama <2430980019@qq.com>
2023-12-21 16:26:22 +0800

    feat: update common.js

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,7 @@
-export function requireImage() {
+export function requireImage(name: string) {
     return new URL(`../assets/images/${name}`, import.meta.url).href
 }
 
-export function requireFile() {
+export function requireFile(name: string) {
     return new URL(`../assets/${name}`, import.meta.url).href
 }
